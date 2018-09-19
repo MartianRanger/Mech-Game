566573a69231a6c97995eee2c1bc0dd105da5d9e
MartianRanger <35358754+MartianRanger@users.noreply.github.com>
2018-09-18 22:06:03 -0600

    Adding Turret Script
    What the title says

---
+++ Mech Game/Assets/Scripts/Turret.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+    Transform player;
+    public Transform gunEnd;
+    public GameObject bullet;
+	// Use this for initialization
+	void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player);
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            StartCoroutine("Shooting");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StopCoroutine("Shooting");
+        }
+    }
+    IEnumerator Shooting()
+    {
+        while (true)
+        {
+            Instantiate(bullet, gunEnd.position, gunEnd.rotation);
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
