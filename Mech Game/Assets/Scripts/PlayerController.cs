a76aa664d4ead21a9d50ac191f8942a0f4532721
CMRoss <ChrisRoss@u.boisestate.edu>
2018-10-06 14:49:55 -0600

    CharacterController F/B/L/R and raycast mouse look

--- Mech Game/Assets/Scripts/PlayerController.cs
+++ Mech Game/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,17 @@ public class PlayerController : NetworkBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public LayerMask layerMask; //where the raycast will hit
+    public float moveSpeed = 2;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 currentLookTarget = Vector3.zero;
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -13,16 +24,58 @@ public class PlayerController : NetworkBehaviour
             return;
         }
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        //transform.Rotate(0, x, 0);
+        characterController.Move(moveDirection * Time.deltaTime * moveSpeed);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetButtonDown("Fire1"))
         {
             CmdFire();
         }
+    }
+
+    //for animation and raycasting
+    public void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (moveDirection == Vector3.zero)
+        {
+            //if the mech is not moving play the the animation for idle
+            //bodyAnimator.SetBool("IsMoving", false);
+        }
+        else
+        {
+            //if the mech is moving play the the animation for walking
+            //bodyAnimator.SetBool("IsMoving", true);
+        }
+
+        RaycastHit hit;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+
+                Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                // 2
+                Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+                // 3
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+            }
+        }
+
     }
 
     // This [Command] code is called on the Client …
@@ -34,7 +87,7 @@ public class PlayerController : NetworkBehaviour
         var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
 
         // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 20;
 
         // Spawn the bullet on the Clients
         NetworkServer.Spawn(bullet);
