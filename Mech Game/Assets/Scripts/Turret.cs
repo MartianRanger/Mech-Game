d8b3e587a57aded6745c770a8e6c7c14a8b2cc26
CMRoss <ChrisRoss@u.boisestate.edu>
2018-11-15 12:16:52 -0700

    Turrets added, detecting player, and looking at the player.transform

--- Mech Game/Assets/Scripts/Turret.cs
+++ Mech Game/Assets/Scripts/Turret.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Turret : NetworkBehaviour {
-    Transform player;
+public class Turret : MonoBehaviour {
+    GameObject player;
     public Transform gunEnd;
     public GameObject bullet;
-	// Use this for initialization
-	void Awake()
+    //Use this for initialization
+
+    void Awake()
     {
-        player = GameObject.FindWithTag("Player").transform;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        transform.LookAt(player);
-	}
+
+
+    //p Update is called once per frame
+    void Update () {
+        
+        //Debug.Log("Player is at " + player.transform.position);
+
+        //player = GameObject.FindWithTag("Player").transform;
+
+
+        if (NetworkManager.singleton.IsClientConnected())
+        {
+            Debug.Log("Client is connected detected");
+            if(GameObject.FindWithTag("Player") != null  && player == null)
+            {
+
+            player = GameObject.FindWithTag("Player");
+            player.gameObject.tag = "Player";
+                Debug.Log("Player found: " + player.name);
+
+            }
+        }
+        if (player != null  && player.transform != null)
+        transform.LookAt(player.transform);
+
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag=="Player")
