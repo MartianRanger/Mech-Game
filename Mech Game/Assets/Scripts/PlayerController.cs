56cfe793461798483e7870fb9baeec9b25207101
MyGriever <35589353+MyGriever@users.noreply.github.com>
2018-11-14 20:34:14 -0700

    Got the mech walking with code

--- Mech Game/Assets/Scripts/PlayerController.cs
+++ Mech Game/Assets/Scripts/PlayerController.cs
@@ -12,14 +12,14 @@ public class PlayerController : NetworkBehaviour
     private Vector3 moveDirection = Vector3.zero;
     private Vector3 currentLookTarget = Vector3.zero;
     private  CharacterController characterController;
-    public Animator mAnimator;
+    private Animator mAnimator;
     public GameObject mLegs;
 
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        mAnimator = mLegs.GetComponentInChildren<Animator>();
+        mAnimator = mLegs.GetComponent<Animator>();
         
     }
 
@@ -27,12 +27,14 @@ public class PlayerController : NetworkBehaviour
     {
         if (!isLocalPlayer)
         {
+            Debug.Log("ejndnas");
             return;
+            
         }
 
         //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
         //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         //transform.Rotate(0, x, 0);
         characterController.Move(moveDirection * Time.deltaTime * moveSpeed);
